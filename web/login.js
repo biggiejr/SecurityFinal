dd23c17d597436fc3475d5b8dd26dbc65a7d5553
biggiejr <jrmacej@gmail.com>
2017-04-24 00:04:41 +0200

    detail

--- web/login.js
+++ web/login.js
@@ -4,14 +4,14 @@ function login() {
     var username = document.getElementById('username').value.toString();
     var password = document.getElementById('password').value.toString();
 
-    if (!escape(username) && !escape(password)){
+    if (!escape(username) && !escape(password)) {
         alert("Login forbidden, due to nasty stuff (special chars)");
         window.location.href = 'http://localhost:8080/Security/index.html';
     }
-        var input = {
-            username: username,
-            password: password
-        };
+    var input = {
+        username: username,
+        password: password
+    };
     $.ajax({
         url: server + 'login',
         headers: {
@@ -31,5 +31,54 @@ function login() {
         }
 
     });
+
+}
+
+function signup() {
+
+    var username = document.getElementById('username').value.toString();
+    var password = document.getElementById('password').value.toString();
+
+    if (!escape(username) && !escape(password)) {
+        alert("Login forbidden, due to nasty stuff (special chars)");
+        window.location.href = 'http://localhost:8080/Security/index.html';
+    }
+    var input = {
+        username: username,
+        password: password
+    };
+
+    function CheckPassword(password)
+    {
+        var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,50}$/;
+        if (inputtxt.value.match(passw))
+        {
+            $.ajax({
+                url: server + 'signup',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                type: 'POST',
+                dataType: 'json',
+                data: JSON.stringify(input),
+                success: function (data) {
+                    var link = data;
+                    window.location.href = 'http://localhost:8080/Security/' + link;
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    console.log(xhr.status);
+                    console.log(thrownError);
+                }
+
+            });
+        }
+        else
+        {
+            alert('insufficient strength of password');
+        }
+    }
+
+
 }
 
